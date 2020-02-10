85586f87e32d8eb08bbb5a7f1267b3ccd620d4c2
Min Xiao <min.xiao@surepayroll.com>
2020-02-10 16:34:23 -0600

    solve more sliding window problems

--- CodePractice/CodePractice/LeetCode/SlidingWindowSubstrings.cs
+++ CodePractice/CodePractice/LeetCode/SlidingWindowSubstrings.cs
@@ -8,14 +8,15 @@ namespace CodePractice.LeetCode
 {
     public class SlidingWindowSubstrings
     {
-        /* Can solove 76. Minimum Window Substring
+        /* Can solove 
+         * 76. Minimum Window Substring
          * 3. Longest Substring Without Repeating Characters
-         * longest-substring-with-at-most-two-distinct-characters
-         * Longest Substring with At Most K Distinct Characters
+         * 159. longest-substring-with-at-most-two-distinct-characters
+         * 340. Longest Substring with At Most K Distinct Characters
          * 30. Substring with Concatenation of All Words
          * 438. Find All Anagrams in a String
          * 567. Permutation in String
-         * Longest Repeating Character Replacement
+         * 424. Longest Repeating Character Replacement
          */
 
         //Template for sliding window
@@ -65,7 +66,7 @@ namespace CodePractice.LeetCode
                     char tempc = s[begin];//***be careful here: choose the char at begin pointer, NOT the end pointer
                     if (map.ContainsKey(tempc))
                     {
-                        map[tempc] = map[tempc] - 1;//plus or minus one
+                        map[tempc] = map[tempc] + 1;//plus or minus one
                         if (map[tempc] > 0) counter++;//modify the counter according the requirement(different condition).
                     }
 
@@ -212,6 +213,7 @@ namespace CodePractice.LeetCode
             return result;
         }
 
+        // 3. Longest Substring Without Repeating Characters
         //sliding approach
         public int LengthOfLongestSubstring(string s)
         {
@@ -226,18 +228,134 @@ namespace CodePractice.LeetCode
                 map[c] = map[c] + 1;
                 right++;
 
-                while(count > 0) //when condition is invalid, we move left to make it valid
+                //since the moment count = 1, it will be catch, so map[c] always equal 2 then, so check == 1 is fine.
+                // a more readable way maybe
+                //first increment
+                // then check if > 1, make it symmetric as left processing.
+                //map[c] = map[c] + 1;
+                //if (map[c] > 1) count++;
+                //right++;
+
+                while (count > 0) //when condition is invalid, we move left to make it valid
                 {
                     char temp = s[left];
-                    if (map[temp] > 1) count--; // the repeating char
+                    if (map[temp] > 1) count--; // the repeating char, count is not for all char, it is only counting repeating characters.
                     map[temp] = map[temp] - 1;
                     left++;
                 }
-                len = Math.Max(len, right - left);  //when we update result, the invariant is satisfied, (no repeating character) within the sliding window. Usually the invariant is just the constraint
+                len = Math.Max(len, right - left);  
+                //when we update result, the invariant is satisfied, (no repeating character) within the sliding window. Usually the invariant is just the constraint
                 //invariant is [left, right) contains a substring without repeating characters, which means every one here is a candidate.
             }
 
             return len;
+        }
+
+        public int LengthOfLongestSubstringTwoDistinct(string s)
+        {
+            int left = 0, right = 0, count = 0;
+            int[] map = new int[256]; // assume all ascii charaters
+            int len = 0;
+
+            while (right < s.Length)
+            {
+                char c = s[right];
+                if (map[c] == 0) count++; //new character, different here, every character counts
+                map[c] = map[c] + 1;
+                right++;
+
+                while (count > 2) //when condition is invalid, we move left to make it valid
+                {
+                    char temp = s[left];
+                    map[temp] = map[temp] - 1;
+                    if (map[temp] == 0) count--; // this character does not exist between (left, right) after left is processed
+                    left++;
+                }
+                len = Math.Max(len, right - left);  
+            }
+
+            return len;
+        }
+
+        //extend to at most K characters
+        public int LengthOfLongestSubstringKDistinct(string s, int k)
+        {
+            int left = 0, right = 0, count = 0;
+            int[] map = new int[256]; // assume all ascii charaters
+            int len = 0;
+
+            while (right < s.Length)
+            {
+                char c = s[right];
+                if (map[c] == 0) count++; //new character, different here, every character counts
+                map[c] = map[c] + 1;
+                right++;
+
+                while (count > k) //when condition is invalid, we move left to make it valid
+                {
+                    char temp = s[left];
+                    map[temp] = map[temp] - 1;
+                    if (map[temp] == 0) count--; // this character does not exist between (left, right) after left is processed
+                    left++;
+                }
+                len = Math.Max(len, right - left);
+            }
+
+            return len;
+        }
+
+
+        public string MinWindow(string s, string p)
+        {
+            if (p.Length > s.Length) return string.Empty;
+
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (freq.ContainsKey(p[i]))
+                    freq[p[i]] = freq[p[i]] + 1;
+                else
+                    freq.Add(p[i], 1);
+            }
+
+            //two pointers, one counter
+            int left = 0, right = 0, counter = freq.Count; // no duplicate character here.
+            int head = 0, minLen = int.MaxValue;
+
+            while (right < s.Length)
+            {
+                if (freq.ContainsKey(s[right]))
+                {
+                    freq[s[right]] = freq[s[right]] - 1;
+
+                    if (freq[s[right]] == 0)
+                        counter--;
+                }
+                right++;
+
+                //next need to move left pointer
+                while (counter == 0)
+                {
+                    if (freq.ContainsKey(s[left]))
+                    {
+                        freq[s[left]] = freq[s[left]] + 1;
+                        //this check is necessary as the value could be still zero
+                        // > 0 means reset to zero,
+                        if (freq[s[left]] > 0)
+                            counter++;
+                    }
+
+                    // all chacters covered plus same length thats a hit
+                    if (right - left < minLen)
+                    {
+                        head = left;
+                        minLen = right - left;
+                    }
+
+                    left++;
+                }
+            }
+            return minLen == int.MaxValue ? string.Empty : s.Substring(head, minLen);
         }
 
     }
