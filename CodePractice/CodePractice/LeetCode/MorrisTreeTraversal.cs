7cd904f8c0e3e30ba227d916e9156c93fa4a15ad
Min Xiao <mxiao3@gmail.com>
2020-02-12 22:12:28 -0600

    tree traversal

---
+++ CodePractice/CodePractice/LeetCode/MorrisTreeTraversal.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice.LeetCode
+{
+    class MorrisTreeTraversal
+    {
+        class Node
+        {
+            public int data;
+            public Node left_node, right_node;
+
+            Node(int item)
+            {
+                data = item;
+                left_node = null;
+                right_node = null;
+            }
+        }
+
+
+        class Tree
+        {
+            Node root;
+
+            void MorrisInOrder(Node root)
+            {
+                Node curr, prev;
+
+                if (root == null)
+                    return;
+
+                curr = root;
+                while (curr != null)
+                {
+                    if (curr.left_node == null)
+                    {
+                        Console.WriteLine(curr.data + " ");
+                        curr = curr.right_node;
+                    }
+                    else
+                    {
+                        /* Find the previous (prev) of curr */
+                        prev = curr.left_node;
+                        while (prev.right_node != null && prev.right_node != curr)
+                            prev = prev.right_node;
+
+                        /* Make curr as right child of its prev */
+                        if (prev.right_node == null)
+                        {
+                            prev.right_node = curr;
+                            curr = curr.left_node;
+                        }
+
+                        /* fix the right child of prev*/
+
+                        else
+                        {
+                            prev.right_node = null;
+                            Console.WriteLine(curr.data + " "); // Visit here, come back to current node
+                            curr = curr.right_node;
+                        }
+
+                    }
+
+                }
+            }
+
+            //public List<Integer> preorderTraversal(TreeNode root)
+            //{
+            //    LinkedList<Integer> output = new LinkedList<>();
+
+            //    TreeNode node = root;
+            //    while (node != null)
+            //    {
+            //        if (node.left == null)
+            //        {
+            //            output.add(node.val); // Visit
+            //            node = node.right;
+            //        }
+            //        else
+            //        {
+            //            TreeNode predecessor = node.left;
+            //            while ((predecessor.right != null) && (predecessor.right != node))
+            //            {
+            //                predecessor = predecessor.right;
+            //            }
+
+            //            if (predecessor.right == null)
+            //            {
+            //                output.add(node.val);  // Visit before come back, before processing.
+            //                predecessor.right = node;
+            //                node = node.left;
+            //            }
+            //            else
+            //            {
+            //                predecessor.right = null; // come back to current
+            //                node = node.right;
+            //            }
+            //        }
+            //    }
+            //}
+
+
+        }
+    }
+}
---
+++ CodePractice/CodePractice/LeetCode/TreeTraversalStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePractice.LeetCode
+{
+    class TreeTraversalStack
+    {
+        //public List<Integer> preorderTraversal(TreeNode root)
+        //{
+        //    LinkedList<TreeNode> stack = new LinkedList<>();
+        //    LinkedList<Integer> output = new LinkedList<>();
+        //    if (root == null)
+        //    {
+        //        return output;
+        //    }
+
+        //    stack.add(root);
+        //    while (!stack.isEmpty())
+        //    {
+        //        //out put before process, inside the stack
+        //        // before process any left or right 
+        //        TreeNode node = stack.pollLast();
+        //        output.add(node.val);
+        //        if (node.right != null)
+        //        {
+        //            stack.add(node.right);
+        //        }
+        //        if (node.left != null)
+        //        {
+        //            stack.add(node.left);
+        //        }
+        //    }
+        //    return output;
+        //}
+
+        //public List<Integer> inorderTraversal(TreeNode root)
+        //{
+        //    List<Integer> res = new ArrayList<>();
+        //    Stack<TreeNode> stack = new Stack<>();
+        //    TreeNode curr = root;
+        //    while (curr != null || !stack.isEmpty())
+        //    {
+        //        while (curr != null)
+        //        {
+        //            stack.push(curr);
+        //            curr = curr.left;
+        //        }
+        //        // left has been processed then we out put
+        //        curr = stack.pop();
+        //        res.add(curr.val);
+        //        curr = curr.right;
+        //    }
+        //    return res;
+        //}
+    }
+}
